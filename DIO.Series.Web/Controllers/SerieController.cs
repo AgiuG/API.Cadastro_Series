c41348747a60b317c59f940410ae5e8d5b8e31b1
AgiuG <guilhermeduarte19@gmail.com>
2021-09-15 20:23:46 -0300

    API Cadastro

---
+++ DIO.Series.Web/Controllers/SerieController.cs
@@ -0,0 +1,65 @@
+﻿using DIO.Series.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Web.Controllers
+{
+    [Route("[Controller]")]
+    public class SerieController : Controller
+    {
+        /*
+         * GET = Retornar algo
+         * POST = Inserir algo
+         * PUT = Alterar algo
+         * DELETE = Excluir algo
+         */
+
+        private readonly IRepositorio<Serie> _repositorioSerie;
+
+        public SerieController(IRepositorio<Serie> repositorioSerie)
+        {
+            _repositorioSerie = repositorioSerie;
+        }
+
+        [HttpGet("")]
+        public IActionResult Lista()
+        {
+            return Ok(_repositorioSerie.Lista().Select(s => new SerieModel(s)));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody] SerieModel model)
+        {
+            _repositorioSerie.Atualiza(id, model.ToSerie());
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Exclui(int id)
+        {
+            _repositorioSerie.Exclui(id);
+            return NoContent();
+        }
+
+        [HttpPost("")]
+        public IActionResult Insere([FromBody] SerieModel model)
+        {
+            model.Id = _repositorioSerie.ProximoId();
+
+            Serie serie = model.ToSerie();
+
+            _repositorioSerie.Insere(model.ToSerie());
+            return Created("", serie);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Consulta(int id)
+        {
+            return Ok(new SerieModel(_repositorioSerie.Lista().FirstOrDefault(s => s.Id == id)));
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# API Cadastro de Series :movie_camera:
+
+Repositório criado para armazenar projeto realizado durante mentoria do Decola Tech 2021.
+
